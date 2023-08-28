f37d3648a4240d7d044565a9ee68f72a13ec6d2c
michael moreno <michaelmoreno@macbook-pro-8.mynetworksettings.com>
2023-08-28 16:48:54 -0400

    created JFrame with title, dimensions, and set visibility to true

---
+++ TestGui.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class TestGui {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Test GUI Example");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400, 300);
+        frame.setVisible(true);
+    }
+}
