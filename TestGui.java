4aefcb6e51a52ad77270831b94d3bbeea9d3c878
michael moreno <michaelmoreno@macbook-pro-8.mynetworksettings.com>
2023-08-28 17:02:37 -0400

    added a label that will be displayed on screen

--- TestGui.java
+++ TestGui.java
@@ -1,11 +1,17 @@
 import javax.swing.*;
-import java.awt.*;
+// import java.awt.*;
 
 public class TestGui {
     public static void main(String[] args) {
         JFrame frame = new JFrame("Test GUI Example");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(400, 300);
+        
+        JLabel label = new JLabel("Hello, Java GUI");
+        frame.add(label);
+
         frame.setVisible(true);
     }
+    
 }
+
